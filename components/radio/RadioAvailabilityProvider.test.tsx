dcab5618fccc019d69ba9822541a3dc55821c9ad
lunamidori5 <118759930+lunamidori5@users.noreply.github.com>
2026-08-31 08:35:28 -0700

    Stabilize radio availability test setup

--- components/radio/RadioAvailabilityProvider.test.tsx
+++ components/radio/RadioAvailabilityProvider.test.tsx
@@ -3,9 +3,6 @@ import { Window } from 'happy-dom';
 import { act } from 'react';
 import { createRoot, type Root } from 'react-dom/client';
 
-process.env.NEXT_PUBLIC_RADIO_AVAILABLE_AT_BUILD = 'true';
-process.env.NEXT_PUBLIC_RADIO_BUILD_ID = 'radio-health-test';
-
 mock.module('next/navigation', () => ({
   usePathname: () => '/radio',
   useRouter: () => ({ replace: () => undefined }),
